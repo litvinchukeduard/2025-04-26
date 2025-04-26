77f0f3a86b184e6f4e9605b52958277a93d9022a
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2025-04-26 20:30:33 +0300

    More vending machine functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import UserList
+from dataclasses import dataclass
 '''
 Написати систему, яка буже керувати торговими апаратами
 '''
@@ -9,7 +10,22 @@ from collections import UserList
 class Product:
     def __init__(self, name: str, price: int):
         self.name = name
+        if price <= 0:
+            raise ValueError("Price can not be less than or equals to 0")
         self.price = price
+
+    def __str__(self):
+        return f"Product(name={self.name}, price={self.price})"
+
+@dataclass(frozen=True)
+class DataProduct:
+    name: str
+    price: int
+    ingredients: list
+
+    def __post_init__(self):
+        if self.price <= 0:
+            raise ValueError("Price can not be less than or equals to 0")
 
 
 # class VendingMachine(UserList):
@@ -23,8 +39,27 @@ class VendingMachine:
             raise ValueError(f"Please check if product {product.name} is not already in a machine")
         self.products.append(product)
 
+    '''
+    Щоб купити продукт, потрібно ввести точну кількість грошей
+    '''
+    def buy_product(self, name: str, amount_of_money: int):
+        result_product = None
+        for product in self.products:
+            if product.name == name:
+                result_product = product
 
-snickers = Product("Snickers", 35)
+        if result_product.price != amount_of_money:
+            raise ValueError('Please enter correct amount of money')
+            
+        self.products.remove(result_product)
+        return result_product
+
+
+# snickers = Product("Snickers", -10)
+data_snicker = DataProduct("Snickers", 10, [1, 2, 3])
+# data_snicker.price = 1000
+# data_snicker.ingredients.append(4)
+print(data_snicker)
 
 # products_set.add(snickers)
 # products_set.add(snickers)
@@ -37,8 +72,11 @@ snickers = Product("Snickers", 35)
 # print(products_set)
 # products = [1]
 machine = VendingMachine()
-machine.add_product(snickers)
-machine.add_product(snickers)
+machine.add_product(data_snicker)
+
+machine.buy_product('Snickers', 10)
+# machine.add_product(snickers)
+# machine.add_product(snickers)
 
 print(machine.products)
 
